4f5922ba89378d5f22f0fea083b5f7517fd309b7
yasmin0204 <164516775+yasmin0204@users.noreply.github.com>
2024-11-01 11:42:18 -0300

    Add files via upload

---
+++ flashcardYasmin/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Música',
+    'Michael Jackson estreou em sua marca registrada moonwalk durante qual música em 1983?',
+    'Billie Jean'
+)
+
+criaCartao(
+    'Filmes',
+    'Este filme de terror é baseado em um agente do FBI (Jodie Foster) tentando usar um serial killer canibal (Anthony Hopkins) com doutorado para ajudar a pegar outro serial killer.',
+    'O Silêncio dos Inocentes'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual país ganhou a Copa do Mundo de 2018?',
+    'França'
+)
+
+criaCartao(
+    'Tecnologia',
+    'Qual é a diferença entre Software e Hardware?',
+    ' Hardware refere-se aos componentes físicos de um sistema, como processadores e discos rígidos, enquanto software é o conjunto de instruções ou programas que operam o hardware.'
+)
